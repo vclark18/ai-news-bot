35f31fbd95f67a724af844022e5332e0e16e7969
vclark18 <vanessa.lee.clark@gmail.com>
2025-06-03 20:45:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ async def on_ready():
     send_daily_news.start()
     send_weekly_digest.start()
 
-@tasks.loop(time=datetime.time(hour=0, minute=45))  # 8:37 PM EST / 1:37 AM UTC
+@tasks.loop(time=datetime.time(hour=0, minute=51))  # 8:37 PM EST / 1:37 AM UTC
 async def send_daily_news():
     for guild in bot.guilds:
         channel = discord.utils.get(guild.text_channels, name=CHANNEL_NAME)
